9943d3420db824af75c7671323c100ffc8918cd9
闵翔 <546143220@qq.com>
2023-05-06 09:57:32 +0800

    master分支删除内容

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import * as tool from './tool'
-import permission from './permission'
-export default {
-    install(Vue){
-       Object.assign(Vue.prototype, tool, {
-         permission
-       })
-    }
-}
