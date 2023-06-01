fedc89cbcfcab7977369257d853152c32d7ba6c8
minxiang51574 <546143220@qq.com>
2023-06-01 21:15:01 +0800

    fix: 解决menu问题

--- src/layout/components/menu.tsx
+++ src/layout/components/menu.tsx
@@ -1,88 +1,71 @@
 /*
  * @Author: Mx
  * @Date: 2023-06-01 12:38:10
- * @Description: 
+ * @Description:
  */
-import { compile, defineComponent, h } from "vue"
-import { useRouter, useRoute, RouteRecordRaw } from "vue-router";
+import { compile, defineComponent, h } from 'vue';
+import { useRouter, useRoute, RouteRecordRaw } from 'vue-router';
 import { appRoutes } from '@/router/routes';
 export default defineComponent({
-    setup(){
-        const router = useRouter();
-        const route = useRoute();
-        // 页面跳转
-		const toView = () => {
-		
-		}
-        const menuTree = appRoutes
-        console.log("menuTree", menuTree);
-        // 渲染子菜单
-        const renderSubMenu = ()=>{
-            function travel(_route: RouteRecordRaw[], nodes = []){
-                    _route.forEach((element) => { 
-                        const node =
-                          element?.children && element?.children.length !== 0 ? (
-                            <el-submenu index={element.path}>
-                                {travel(element.children)}
-                            </el-submenu>
-                          ) : (
-                            <el-menu-item index={element.path}>
-                                { element.name }
-                            </el-menu-item>
-                          );
-                        nodes.push(node as never);
-                    });
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+    // 页面跳转
+    const toView = () => {};
+    const menuTree = appRoutes;
 
-                return nodes
-            }
-            return travel(menuTree);
+    // 渲染子菜单
+    const renderSubMenu = () => {
+      function travel(menuList: RouteRecordRaw[]) {
+        return menuList.map((item: any) => {
+          // 有子菜单
+          if (item?.children && item?.children?.length > 0) {
+            const slots = {
+              title: () => item.name,
+            };
+            return (
+              <el-sub-menu index={item.path} v-slots={slots}>
+                {getMenu(item?.children)}
+              </el-sub-menu>
+            );
+          } else {
+            // 没有子菜单
+            return <el-menu-item index={item.path}>{item.name}</el-menu-item>;
+          }
+        });
+      }
+      return travel(menuTree);
+    };
+
+    //菜单生成
+    const getMenu = (menuList: any) => {
+      return menuList.map((item: any) => {
+        if (item?.children && item?.children?.length > 0) {
+          const slots = {
+            title: () => item.name,
+          };
+          return (
+            <el-sub-menu index={item.path} v-slots={slots}>
+              {getMenu(item?.children)}
+            </el-sub-menu>
+          );
+        } else {
+          return <el-menu-item index={item.path}>{item.name}</el-menu-item>;
         }
+      });
+    };
 
-        //菜单生成
-        const getMenu = (menuList: any) => {
-            return menuList.map((item: any) => {
-                if (item?.children && item?.children?.length > 0) {
-                    const slots = {
-                        title: () => (
-                            item.name
-                        ),
-                    }
-                    return (
-                        <el-sub-menu index={item.path} v-slots={slots}>
-                            { getMenu(item?.children) }
-                        </el-sub-menu>
-                    )
-                } else {
-                    return (
-                    <el-menu-item index={item.path}>
-                        {item.name}
-                    </el-menu-item>
-                    )
-                }
-            })
-        }
-  
-        return ()=>(
-            <div class="app-slider__menu">
-                <el-menu
-						default-active={route.path}
-						background-color="transparent"
-						collapse-transition={true}
-						collapse={true}
-						onSelect={toView}
-					>
-						{/* {renderSubMenu()} */}
-                         {/* {getMenu(menuTree)} */}
-                         <el-submenu index="1">
-                            <el-menu-item index="1-1">item one</el-menu-item>
-                            <el-menu-item index="1-2">item two</el-menu-item>
-                        </el-submenu>
-                        <el-menu-item index="1-1">item one</el-menu-item>
-                        <el-menu-item index="1-2">item two</el-menu-item>
-                    </el-menu>
-            </div>
-        )
-
-    }
-
-})
+    return () => (
+      <div class="app-slider__menu">
+        <el-menu
+          default-active={route.path}
+          background-color="transparent"
+          collapse-transition={true}
+          onSelect={toView}
+        >
+          {renderSubMenu()}
+        </el-menu>
+      </div>
+    );
+  },
+});
--- src/layout/components/slider.vue
+++ src/layout/components/slider.vue
@@ -8,29 +8,6 @@
     <div class="app-slider__container">
       <el-scrollbar wrap-class="scrollbar-wrapper">
         <Menu />
-        <!-- <el-menu default-active="2" class="el-menu-vertical-demo">
-          <el-sub-menu index="1">
-            <template #title>
-              <span>Navigator One</span>
-            </template>
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-            <el-menu-item index="1-3">item three</el-menu-item>
-            <el-sub-menu index="1-4">
-              <template #title>item four</template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-menu-item index="2">
-            <span>Navigator Two</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <span>Navigator Three</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <span>Navigator Four</span>
-          </el-menu-item>
-        </el-menu> -->
       </el-scrollbar>
     </div>
   </div>
@@ -46,7 +23,6 @@ const route = useRoute();
 const isCollapse = computed(() => true);
 const menuStore = useMenuStore();
 const menuList = menuStore.menuList;
-console.log(menuList);
 </script>
 <style lang="scss">
 .app-slider {
